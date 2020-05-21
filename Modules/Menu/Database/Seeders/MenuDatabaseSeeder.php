03abd4770185b4de1932e642d786c94da1d9f823
umaams <khotib.umam7@gmail.com>
2020-05-21 12:30:24 +0700

    Add module menu

--- Modules/Menu/Database/Seeders/MenuDatabaseSeeder.php
+++ Modules/Menu/Database/Seeders/MenuDatabaseSeeder.php
@@ -21,10 +21,5 @@ class MenuDatabaseSeeder extends Seeder
         $permission = Menu::create(['name' => 'Permissions', 'link' => 'permissions', 'parent_menu_id' => $auth->id]);
         $role = Menu::create(['name' => 'Roles', 'link' => 'roles', 'parent_menu_id' => $auth->id]);
         $user = Menu::create(['name' => 'Users', 'link' => 'users', 'parent_menu_id' => $auth->id]);
-        $manufacture = Menu::create(['name' => 'Manufacture', 'link' => '#', 'item_order' => '2', 'parent_menu_id' => 0]);
-        $material = Menu::create(['name' => 'Materials', 'link' => 'materials', 'parent_menu_id' => $manufacture->id]);
-        $customer = Menu::create(['name' => 'Customers', 'link' => 'customers', 'parent_menu_id' => $manufacture->id]);
-        $routing = Menu::create(['name' => 'Routings', 'link' => 'routings', 'parent_menu_id' => $manufacture->id]);
-        $bom = Menu::create(['name' => 'Bill of Materials', 'link' => 'boms', 'parent_menu_id' => $manufacture->id]);
     }
 }
